d7f27b15ad0f2a143a88a76c103338a870d784e3
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-02 15:19:27 +0100

    Add generic singleton script

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : Component {
+
+    private static T instance;
+    public static T Instance {
+        get {
+            if (instance == null) {
+                var objs = FindObjectsOfType<T>() as T[];
+
+                if (objs.Length > 0) {
+                    instance = objs[0];
+                }
+
+                if (objs.Length > 1) {
+                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene!");
+                }
+
+                if (instance == null) {
+                    GameObject obj = new GameObject {
+                        name = typeof(T).Name,
+                        hideFlags = HideFlags.HideAndDontSave
+                    };
+
+                    instance = obj.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+}
