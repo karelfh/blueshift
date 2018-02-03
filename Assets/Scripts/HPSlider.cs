a187af5e2338cd415c901fbb0197572ffbcb2cfd
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-03 18:07:24 +0100

    Add player meteor damage and player death

---
+++ Assets/Scripts/HPSlider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HPSlider : MonoBehaviour {
+
+    [SerializeField] private FloatReferece playerHealth;
+
+    private Slider hpSlider;
+
+
+    private void Awake() {
+        hpSlider = GetComponent<Slider>();
+    }
+
+    private void Update() {
+        hpSlider.value = playerHealth.Value;
+    }
+
+}
