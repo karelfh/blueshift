afe343e52293a46d0b62687206dcabbbd3f37040
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-03 18:06:52 +0100

    Add player score

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    [SerializeField] private IntReference playerScore;
+
+    private Text scoreText;
+
+
+    private void Awake() {
+        scoreText = GetComponent<Text>();
+    }
+
+    private void Update() {
+        scoreText.text = playerScore.Value.ToString();
+    }
+
+}
