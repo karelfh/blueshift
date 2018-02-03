88c3d563751b653bc8bc5f57c244bb7afe0346c9
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-03 10:59:47 +0100

    Add object movement and destroyer script
    Destroyer script - deactivate objects that pass through it and make them usable again in object pooler.

---
+++ Assets/Sandbox/ObjectMovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovementController : MonoBehaviour {
+
+    [SerializeField] private float movementSpeed;
+
+    private Vector2 movement;
+
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        movement.Set(0, -1);
+        movement = movement.normalized * movementSpeed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+
+}
---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(10, 1, 1));
+    }
+
+}
