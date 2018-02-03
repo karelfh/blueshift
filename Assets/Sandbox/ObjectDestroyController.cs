04402e444d4db40eb3c56a8055c4088770d9364b
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-03 14:35:19 +0100

    Add collision controller

--- Assets/Sandbox/ObjectDestroyController.cs
+++ Assets/Sandbox/ObjectDestroyController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObjectDestroyController : MonoBehaviour {
 
@@ -11,7 +9,7 @@ public class ObjectDestroyController : MonoBehaviour {
         }
     }
 
-    private void Destroy() {
+    public void Destroy() {
         gameObject.SetActive(false);
     }
 
---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CollisionController : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.CompareTag("Meteor")) {
+            Debug.Log("Hit by: " + coll.gameObject.name);
+
+            // Destroy meteor, get damage, die if health <= 0
+            coll.GetComponent<ObjectDestroyController>().Destroy();
+        }
+
+        if (coll.CompareTag("Star")) {
+            Debug.Log("Collected: " + coll.gameObject.name);
+
+            // Destroy star, get score, reload 1 missile
+            coll.GetComponent<ObjectDestroyController>().Destroy();
+        }
+    }
+
+}
