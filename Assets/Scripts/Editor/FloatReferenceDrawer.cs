ff3b766afc40a5b356fe96335ef37bc164a05ad3
Karel-František Houf <EldrikCZ@gmail.com>
2018-02-04 15:18:41 +0100

    Fix major performace bug
    Fix bug where Gfx.WaitForPresent (see unity profiler) caused large fps spikes. (Loaded previous workings asset files)

--- Assets/Scripts/Editor/FloatReferenceDrawer.cs
+++ Assets/Scripts/Editor/FloatReferenceDrawer.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(IntReference))]
-public class IntReferenceEditor : PropertyDrawer {
+[CustomPropertyDrawer(typeof(FloatReference))]
+public class FloatReferenceDrawer : PropertyDrawer {
+
     /// <summary>
     /// Options to display in the popup to select constant or variable.
     /// </summary>
     private readonly string[] popupOptions =
         { "Use Constant", "Use Variable" };
 
-    /// <summary> Cached style to use to draw the popup button. </summary>
+    /// <summary>
+    /// Cached style to use to draw the popup button.
+    /// </summary>
     private GUIStyle popupStyle;
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
@@ -38,15 +41,15 @@ public class IntReferenceEditor : PropertyDrawer {
         // Store old indent level and set it to 0, the PrefixLabel takes care of it
         int indent = EditorGUI.indentLevel;
         EditorGUI.indentLevel = 0;
-        
+
         int result = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 0 : 1, popupOptions, popupStyle);
 
         useConstant.boolValue = result == 0;
 
         EditorGUI.PropertyField(position,
-                                useConstant.boolValue ? constantValue : variable,
-                                GUIContent.none);
-        
+            useConstant.boolValue ? constantValue : variable,
+            GUIContent.none);
+
         if (EditorGUI.EndChangeCheck())
             property.serializedObject.ApplyModifiedProperties();
 
---
+++ Assets/Scripts/Player Controllers/CollisionController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CollisionController : MonoBehaviour {
+
+    [Header("Damage Settings")]
+    [SerializeField] private FloatVariable playerHealth;
+    [SerializeField] private FloatReference meteorDamageSmall;
+
+    [Header("Score Settings")]
+    [SerializeField] private FloatVariable playerScore;
+    [SerializeField] private FloatReference starScoreValue;
+
+
+    private void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.CompareTag("Meteor")) {
+            // Destroy meteor, get damage
+            playerHealth.Value -= meteorDamageSmall.Value;
+            coll.GetComponent<ObjectDestroyController>().Destroy();
+        }
+
+        if (playerHealth.Value <= 0f) {
+            Debug.Log("You Died!");
+            Destroy(gameObject);
+        }
+
+        if (coll.CompareTag("Star")) {
+            // Destroy star, get score, reload 1 missile
+            playerScore.Value += starScoreValue.Value;
+            coll.GetComponent<ObjectDestroyController>().Destroy();
+        }
+    }
+
+}
