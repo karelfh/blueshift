5c5ad1db031410b1541c4d96c025caab138addcb
Karel-František Houf <EldrikCZ@gmail.com>
2018-03-02 18:06:18 +0100

    Remove slowdown time when player hit by asteroid
    Script was causing a lot of lag.

--- Assets/Scripts/Game Controllers/TimeManager.cs
+++ Assets/Scripts/Game Controllers/TimeManager.cs
@@ -2,12 +2,6 @@
 
 public class TimeManager : MonoBehaviour {
 
-    [Header("Slowdown Settings")]
-    [Tooltip("How much will time be slowed.")]
-    [SerializeField] private FloatReference slowdownFactor;
-    [Tooltip("For how long, in seconds, will time be slowed.")]
-    [SerializeField] private FloatReference slowdownLength;
-
     private bool isGameStarted = false;
     private bool isGameOver = false;
 
@@ -16,20 +10,12 @@ public class TimeManager : MonoBehaviour {
         if (!isGameStarted) {
             Time.timeScale = 0f;
         } else {
-            if (!isGameOver && isGameStarted) {
-                Time.timeScale += (1 / slowdownLength) * Time.unscaledDeltaTime;
-                Time.timeScale = Mathf.Clamp(Time.timeScale, 0, 1);
-            } else {
+            Time.timeScale = 1f;
+
+            if (isGameOver) {
                 Time.timeScale = 0f;
             }
         }
-
-        
-    }
-
-    public void SlowdownTime() {
-        Time.timeScale = slowdownFactor;
-        Time.fixedDeltaTime = Time.timeScale * .02f;
     }
 
     public void IsGameOver(bool _isGameOver) {
